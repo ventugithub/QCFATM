bd0075d8ab1c8e16695cec8bccc3b09d5eca2ad2
Tobias Stollenwerk <tobias.stollenwerk@dlr.de>
2016-04-26 11:00:12 -0700

    Fixed error in point conflict calculation
    Added documentation, cleaned up code, visualize point conflicts

--- precalculation/atm.py
+++ precalculation/atm.py
@@ -1,121 +1,51 @@
+#!/usr/bin/env python
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import itertools
 import conflict
 import os
-from mpl_toolkits.basemap import Basemap
+import argparse
 
-def plotTrajectories(trajectories):
-    # Create a figure of size (i.e. pretty big)
-    plt.figure(figsize=(20, 10))
+def main():
+    parser = argparse.ArgumentParser(description='Calculate point conflicts from trajectory data')
+    parser.add_argument('-i', '--input', default='data/TrajDataV2_20120729.txt', help='input file containing the trajectory data')
+    parser.add_argument('-o', '--output', default='pointConflicts.dat', help='output file')
+    parser.add_argument('-d', '--mindistance', default=30, help='Minimum distance in nautic miles to qualify as a conflict')
+    parser.add_argument('-t', '--mintime', default=60, help='Minimum time difference in minutes to qualify as a conflict')
+    parser.add_argument('--overwrite', action='store_true', help='Force recalulation of intermediate step of caluclatin consecutive flight indices in the data')
+    args = parser.parse_args()
 
-    # Create a map, using the Gall-Peters projection,
-    map = Basemap(projection='gall',
-                  # with low resolution,
-                  resolution='l',
-                  # And threshold 100000
-                  area_thresh=100000.0,
-                  # Centered at 0,0 (i.e null island)
-                  lat_0=0, lon_0=0)
+    # nautic mile in kilometers
+    nautic = 1.852
 
-    # Draw the coastlines on the map
-    map.drawcoastlines()
+    # minimal acceptable distance in kilometers
+    mindistance = args.mindistance * nautic
 
-    # Draw country borders on the map
-    map.drawcountries()
+    # minimal acceptable time difference
+    mintime = args.mintime
 
-    # Fill the land with grey
-    map.fillcontinents(color='#888888')
+    inputDataFile = args.input
+    filename = inputDataFile + ".h5"
+    trajectories = None
+    if not os.path.exists(filename) or args.overwrite:
+        print "Read in trajectories ..."
+        trajectories = pd.read_csv(inputDataFile,
+                                   delimiter="\t",
+                                   names=('flight', 'date', 'wind', 'time', 'speed', 'altitude', 'latitude', 'longitude', 'nan'),
+                                   usecols=('flight', 'date', 'wind', 'time', 'speed', 'altitude', 'latitude', 'longitude')
+                                   )
+        print "Add consecutive flight index to data ..."
+        # add consecutive flight index to the data
+        flightNames = trajectories['flight'].unique()
+        trajectories['flightIndex'] = trajectories['flight'].map(lambda x: np.where(flightNames == x)[0][0])
+        # set consecutive flight index as dataset index
+        trajectories = trajectories.set_index('flightIndex')
+        trajectories.to_hdf(filename, 'trajectories', mode='w')
+    else:
+        print "read in trajectories ..."
+        trajectories = pd.read_hdf(filename, 'trajectories')
 
-    # Draw the map boundaries
-    map.drawmapboundary(fill_color='#f4f4f4')
+    # calulate point conflicts
+    conflict.detectConflicts(trajectories.index, trajectories.time, trajectories.latitude, trajectories.longitude, args.output, mindistance, mintime)
 
-    # Define our longitude and latitude points
-    # We have to use .values because of a wierd bug when passing pandas data
-    # to basemap.
-    x, y = map(trajectories['longitude'].values, trajectories['latitude'].values)
-
-    # Plot them using round markers of size 6
-    map.plot(x, y, 'b', markersize=6)
-
-    # Show the map
-    plt.show()
-
-
-def distance(lat1, lon1, lat2, lon2, R=6367):
-    """Get the distance on a great circle between to trajectory points in kilometers
-
-    Arguments:
-    R: Radius in kilometers
-    lat1: latitude of first point in degrees
-    lon1: longitude of the first point in degrees
-    lat2: latitude of the second point in degrees
-    lon2: longitude of the second point in degrees
-    """
-    Lat0 = np.radians(lat1)
-    Latf = np.radians(lat2)
-    Lon0 = np.radians(lon1)
-    Lonf = np.radians(lon2)
-
-    return R * np.arccos(np.sin(Lat0) * np.sin(Latf) + np.cos(Lonf-Lon0)*np.cos(Lat0)*np.cos(Latf))
-
-def detectSpatialConflict(lat1, lon1, lat2, lon2, mindistance):
-    return distance(lat1, lon1, lat2, lon2) < mindistance
-
-
-# constants
-# nautic mile in kilometers
-nautic = 1.852
-
-# minimal acceptable distance in kilometers
-mindistance = 30 * nautic
-
-# minimal acceptable time difference
-mintime = 3
-
-inputDataFile = "data/TrajDataV2_20120729.txt"
-filename = 'trajectories'
-trajectories = None
-if not os.path.exists(filename + '.h5'):
-    # read in data from July, 29th, 2012
-    print "read in trajectories ..."
-    trajectories = pd.read_csv(inputDataFile,
-                               delimiter="\t",
-                               names=('flight', 'date', 'wind', 'time', 'speed', 'altitude', 'latitude', 'longitude', 'nan'),
-                               usecols=('flight', 'date', 'wind', 'time', 'speed', 'altitude', 'latitude', 'longitude')
-                               )
-    print "add consecutive flight index to data ..."
-    # add consecutive flight index to the data
-    flightNames = trajectories['flight'].unique()
-    trajectories['flightIndex'] = trajectories['flight'].map(lambda x: np.where(flightNames == x)[0][0])
-    # set consecutive flight index as dataset index
-    trajectories = trajectories.set_index('flightIndex')
-    trajectories.to_hdf(filename + '.h5', 'trajectories', mode='w')
-else:
-    print "read in trajectories ..."
-    trajectories = pd.read_hdf(filename + '.h5', 'trajectories')
-
-conflict.detectConflicts(trajectories.index, trajectories.time, trajectories.latitude, trajectories.longitude)
-
-# print "detect conflicts ..."
-# t = trajectories[trajectories.time == 19]
-# flightPairs = np.array(list(itertools.combinations(t.index, 2)))
-# df = pd.DataFrame(flightPairs, columns=['first', 'second'])
-# df['isConflict'] = df.apply(lambda x:
-                            # detectSpatialConflict(t['latitude'][x['first']],
-                                                  # t['longitude'][x['first']],
-                                                  # t['latitude'][x['second']],
-                                                  # t['longitude'][x['second']],
-                                                  # mindistance),
-                            # axis=1)
-
-
-# print np.array(trajectories.index).min()
-# print np.array(trajectories.index).max()
-# print "loop all conflicts ..."
-# n = 0
-# for (i, j) in itertools.combinations(trajectories.index.unique(), 2):
-    # n = n + 1
-    # if n % 10000 == 0:
-        # print i, j
+if __name__ == "__main__":
+        main()
--- precalculation/conflict.pyx
+++ precalculation/conflict.pyx
@@ -3,14 +3,61 @@ import cython
 import math
 from libc.math cimport sin, cos, acos, fabs
 from libcpp.vector cimport vector
+import progressbar
+
 
 cdef mapToCoarseGrid(float lat, float lon, float time, float latMin, float lonMin, float timeMin, float deltaLat=0.5, float deltaLon=0.5, float deltaTime=60.0):
+    """ map trajectory point to coarse grid
+
+    Arguments:
+    lat: latitude of the input trajectory point
+    lon: longitude of the input trajectory point
+    time: time of the input trajectory point
+    latMin: minimum latitude of all trajectory points
+    lonMin: minimum longitude of all trajectory points
+    timeMin: minimum time of all trajectory points
+    deltaLat: coarse grid step in latitude direction in degrees
+    deltaLon: coarse grid step in longitude direction in degrees
+    deltaTime: coarse grid step in in time
+
+    returns: (I, J, K)
+    I: coarse grid index in latitude direction
+    J: coarse grid index in longitude direction
+    K: coarse grid index in time direction
+    """
     cdef int I = int((lat - latMin) / deltaLat)
     cdef int J = int((lon - lonMin) / deltaLon)
     cdef int T = int((time - timeMin) / deltaTime)
     return I, J, T
 
 cdef getCoarsePointConflict(vector[vector[vector[vector[vector[float]]]]] & coarseTraj, int I, int J, int K):
+    """
+    Given a coarse grid cell (I, J, K), return all neighboring
+    coarse grid cells which contain trajactory points
+
+    Arguments:
+    coarseTraj: a mapping from each trajectory point to
+                a coarse grid. It is designed as follows:
+                Dimension | Content
+                    1     | coarse grid index in latitude direction
+                    2     | coarse grid index in longitude direction
+                    3     | coarse grid index in time direction
+                    4     | 5-dimensional trajectory point information
+                          |  1. consecutive flight index
+                          |  2. row index of the original trajectory data
+                          |  3. exact latitude
+                          |  4. exact longitude
+                          |  5. exact time
+                    5     | index indicating the different trajectory points
+                          | inside a coarse grid cell
+                Example: coarseTraj(3, 4, 5, 2, 15) is the exact latitude of
+                the 15th trajectory point in the coarse grid cell (3, 4, 5).
+                I.e. with latitude index 3, longitude index 4, and time index 5
+
+    I: coarse grid index in latitude direction
+    J: coarse grid index in longitude direction
+    K: coarse grid index in time direction
+    """
     rangeI = np.array((I - 1, I, I + 1), dtype=int)
     rangeJ = np.array((J - 1, J, J + 1), dtype=int)
     rangeK = np.array((K - 1, K, K + 1), dtype=int)
@@ -25,10 +72,23 @@ cdef getCoarsePointConflict(vector[vector[vector[vector[vector[float]]]]] & coar
                     conflicts.append((i, j, k))
     return conflicts
 
-
-
-
 cdef getPointConflict(float lat1, float lon1, float time1, float lat2, float lon2, float time2, float spaceThreshold=55.56, float timeThreshold=60.0, earthRadius=6000):
+    """ Given two trajectory points (lat1, lon1, time1) and (lat2, lon2, time2)
+    calculate if there is a conflict
+
+    Arguments:
+
+    lat1: latitude of the first trajectory point
+    lon1: longitude of the first trajectory point
+    time1: time of the first trajectory point
+    lat2: latitude of the second trajectory point
+    lon2: longitude of the second trajectory point
+    time2: time of the second trajectory point
+    spaceThreshold: minimal distance in kilometer to avoid conflict
+    timeThreshold: minimal time difference in minutes to avoid conflict
+    earthRadius: earth radius in kilometer
+    """
+
     CosD = sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * (cos(lon1) * cos(lon2) + sin(lon1) * sin(lon2))
     CosD = min(CosD, 1)
     CosD = max(CosD, -1)
@@ -39,7 +99,7 @@ cdef getPointConflict(float lat1, float lon1, float time1, float lat2, float lon
     else:
         return False
 
-def detectConflicts(flightIndices, times, lat, lon):
+def detectConflicts(flightIndices, times, lat, lon, pointConflictFile, mindistance, mintime):
     """ Detect conflicts
 
     Arguments:
@@ -47,33 +107,57 @@ def detectConflicts(flightIndices, times, lat, lon):
     times: array of trajectory times (same length as flightIndices)
     lat: array of trajectory latitudes (same length as flightIndices)
     lon: array of trajectory longitudes (same length as flightIndices)
+    pointConflictFile: output file name
+    mindistance: minimum distance in nautic miles to qualify as a conflict
+    mintime: minimum time difference in minutes to qualify as a conflict
     """
-    flights = np.array(flightIndices, dtype=int)
-    times = np.array(times, dtype=int)
-    lat = np.array(lat, dtype=float)
-    lon = np.array(lon, dtype=float)
-    cdef float latMin = lat.min()
-    cdef float latMax = lat.max()
-    cdef float lonMin = lon.min()
-    cdef float lonMax = lon.max()
-    cdef float timeMin = times.min()
-    cdef float timeMax = times.max()
+    #######################################################
+    # constants
+    #######################################################
+    # grid discretization for latitude
     cdef float deltaLat = 0.5
+    # grid discretization for longitude
     cdef float deltaLon = 0.5
+    # grid discretizaiton for time
     cdef int deltaTime = 60
-    cdef int Nlat = int((latMax - latMin) / deltaLat) + 1
-    cdef int Nlon = int((lonMax - lonMin) / deltaLon) + 1
-    cdef int Ntime = int((timeMax - timeMin) / deltaTime) + 1
-
     # earth radius in kilometers
     cdef float earthRadius = 6367.0
-    # nautic mile in kilometers
-    cdef float nautic = 1.852
     # minimal acceptable distance in kilometers
-    cdef float spaceThreshold = 30.0 * nautic
+    cdef float spaceThreshold = mindistance
     # minimal acceptable time difference in minutes
-    cdef float temporalThreshold = 60
+    cdef float temporalThreshold = mintime
 
+
+    # consecutive flight indices
+    flights = np.array(flightIndices, dtype=int)
+    # times in minutes
+    times = np.array(times, dtype=int)
+    # latitude
+    lat = np.array(lat, dtype=float)
+    # longitude
+    lon = np.array(lon, dtype=float)
+    # minimal latitude value
+    cdef float latMin = lat.min()
+    # maximal latitude value
+    cdef float latMax = lat.max()
+    # minimal longitude value
+    cdef float lonMin = lon.min()
+    # maximal longitude value
+    cdef float lonMax = lon.max()
+    # minimal time value
+    cdef float timeMin = times.min()
+    # maximal time value
+    cdef float timeMax = times.max()
+    # number of grid points in latitude direction
+    cdef int Nlat = int((latMax - latMin) / deltaLat) + 1
+    # number of grid points in longitude direction
+    cdef int Nlon = int((lonMax - lonMin) / deltaLon) + 1
+    # number of grid points in time
+    cdef int Ntime = int((timeMax - timeMin) / deltaTime) + 1
+    # number of trajectory points
+    cdef int N = len(flightIndices)
+
+    # cython typed variable definitions (for speed)
     cdef int i
     cdef int n = 0
     cdef int m = 0
@@ -87,8 +171,42 @@ def detectConflicts(flightIndices, times, lat, lon):
     cdef int c = 0
     cdef int flight1
     cdef int flight2
-    print "prepare coarse trajectory container"
-    #coarseTraj = np.empty((Nlat, Nlon, Ntime, 0))
+    cdef int I
+    cdef int J
+    cdef int K
+    cdef int Ip
+    cdef int Jp
+    cdef int Kp
+    cdef float lat1
+    cdef float lon1
+    cdef float time1
+    cdef float lat2
+    cdef float lon2
+    cdef float time2
+
+    #########################################################
+    ##### prepare coarse trajectory grid container ##########
+    #########################################################
+    # coarseTraj is a mapping from each trajectory point to
+    # a coarse grid. It is designed as follows:
+    # Dimension | Content
+    #     1     | coarse grid index in latitude direction
+    #     2     | coarse grid index in longitude direction
+    #     3     | coarse grid index in time direction
+    #     4     | 5-dimensional trajectory point information
+    #           |  1. consecutive flight index
+    #           |  2. row index of the original trajectory data
+    #           |  3. exact latitude
+    #           |  4. exact longitude
+    #           |  5. exact time
+    #     5     | index indicating the different trajectory points
+    #           | inside a coarse grid cell
+    #
+    #
+    # Example: coarseTraj(3, 4, 5, 2, 15) is the exact latitude of
+    # the 15th trajectory point in the coarse grid cell (3, 4, 5).
+    # I.e. with latitude index 3, longitude index 4, and time index 5
+    print "Prepare coarse trajectory container ..."
     cdef vector[vector[vector[vector[vector[float]]]]] coarseTraj
     coarseTraj.resize(Nlat)
     for i in range(0, Nlat):
@@ -98,26 +216,14 @@ def detectConflicts(flightIndices, times, lat, lon):
             for k in range(0, Ntime):
                 coarseTraj[i][j][k].resize(5)
 
-    N = len(flightIndices)
-    flightNumbers = np.unique(flights)
-    NFlights = len(flightNumbers)
 
-    # get the separation of flight indices
-    cdef int I
-    cdef int J
-    cdef int K
-    cdef int Ip
-    cdef int Jp
-    cdef int Kp
-    ib = 0
-    flightIndexLimits = np.ndarray(NFlights + 1, dtype=int)
-    flightIndexLimits[0] = 0
-    flightIndexLimits[NFlights] = len(flights)
-    print "mapping to coarse trajectory container"
-    print Nlat, Nlon, Ntime
+    ###################################################
+    #### mapping to coarse trajectory container #######
+    ###################################################
+    print "Map trajectory point to coarse grid ..."
     for i in range(1, len(flights)):
-        if i % 1000 == 0:
-            print i, " of ",  len(flights)
+        #if i % 1000 == 0:
+            #print i, " of ",  len(flights)
         # mapping to coarse trajectory container
         I, J, K = mapToCoarseGrid(lat[i], lon[i], times[i], latMin, lonMin, timeMin, deltaLat, deltaLon, deltaTime)
         coarseTraj[I][J][K][0].push_back(flights[i])
@@ -126,47 +232,52 @@ def detectConflicts(flightIndices, times, lat, lon):
         coarseTraj[I][J][K][3].push_back(lon[i])
         coarseTraj[I][J][K][4].push_back(times[i])
 
-        # get flight index limits
-        if (flights[i] == flights[ib] + 1):
-            flightIndexLimits[flights[i]] = i
-            ib = i
-
-    f = open('pointConflicts.dat', 'w')
+    ##################################################
+    ### calculate point conflicts ####################
+    ##################################################
+    f = open(pointConflictFile, 'w')
     f.write('# conflictIndex, flightIndex1, flightIndex2, lat1, lon1, time1, lat2, lon2, time2\n')
-    n = 0
-
-    cdef float lat1
-    cdef float lon1
-    cdef float time1
-    cdef float lat2
-    cdef float lon2
-    cdef float time2
+    # conflict number
     c = 0
+    # progress bar
+    Nloops = (Nlat - 1) * (Nlon - 1) * (Ntime - 1)
+    print 'Calculate point conflicts'
+    pbar = progressbar.ProgressBar().start()
+    pbar.maxval = Nloops
+    # looping over all coarse grid cells
+    n = 0
     for I in range(1, Nlat - 1):
         for J in range(1, Nlon - 1):
             for K in range(1, Ntime - 1):
-                if n % 100 == 0:
-                    print n, " of ", (Nlat - 1) * (Nlon - 1) * (Ntime - 1)
+                if n % 10000 == 0:
+                    pbar.update(n)
                 n = n + 1
+                # get all coarse grid cells in vicinity of the
+                # current coarse grid cell (I, J, K) which contain
+                # trajectory point as a list of 3-tuples (i, j, k)
                 conflicts = getCoarsePointConflict(coarseTraj, I, J, K)
                 if conflicts:
-                    for l in range(coarseTraj[I][J][K][0].size()):
+                    # loop over all trajectory point in the current coarse grid cell
+                    for l in range(int(coarseTraj[I][J][K][0].size())):
                         flight1 = int (coarseTraj[I][J][K][0][l])
                         lat1 = coarseTraj[I][J][K][2][l]
                         lon1 = coarseTraj[I][J][K][3][l]
                         time1 = coarseTraj[I][J][K][4][l]
-                        m = 0
+                        # loop over all trajectory points in the neigboring coarse gri cells
                         for con in conflicts:
                             Ip = con[0]
                             Jp = con[1]
                             Kp = con[2]
-                            flight2 = int(coarseTraj[Ip][Jp][Kp][0][m])
-                            lat2 = coarseTraj[Ip][Jp][Kp][2][m]
-                            lon2 = coarseTraj[Ip][Jp][Kp][3][m]
-                            time2 = coarseTraj[Ip][Jp][Kp][4][m]
-                            if flight1 != flight2:
-                                isConflict = getPointConflict(lat1, lon1, time1, lat2, lon2, time2, spaceThreshold, temporalThreshold, earthRadius)
-                                m = m + 1
-                                if isConflict:
-                                    f.write('%i, %i, %i, %f, %f, %f, %f, %f, %f\n' % (c, coarseTraj[I][J][K][0][l], coarseTraj[Ip][Jp][Kp][0][m], lat1, lon1, time1, lat2, lon2, time2))
-                                    c = c + 1
+                            for m in range(coarseTraj[Ip][Jp][Kp][0].size()):
+                                flight2 = int(coarseTraj[Ip][Jp][Kp][0][m])
+                                lat2 = coarseTraj[Ip][Jp][Kp][2][m]
+                                lon2 = coarseTraj[Ip][Jp][Kp][3][m]
+                                time2 = coarseTraj[Ip][Jp][Kp][4][m]
+                                # if the flight number is different, check if there is a point conflict and write the information to a text file
+                                if flight1 != flight2:
+                                    isConflict = getPointConflict(lat1, lon1, time1, lat2, lon2, time2, spaceThreshold, temporalThreshold, earthRadius)
+                                    if isConflict:
+                                        f.write('%i, %i, %i, %f, %f, %f, %f, %f, %f\n' % (c, coarseTraj[I][J][K][0][l], coarseTraj[Ip][Jp][Kp][0][m], lat1, lon1, time1, lat2, lon2, time2))
+                                        c = c + 1
+    pbar.finish()
+    print "Point conflict data written to", pointConflictFile
