3df0a85cf075354a259b4f625364c932fbdb387e
Tobias Stollenwerk <tobias.stollenwerk@dlr.de>
2016-05-05 16:30:14 -0700

    Added graph plotting in precalculation

--- precalculation/analysis.py
+++ precalculation/analysis.py
@@ -5,6 +5,7 @@ import numpy as np
 import tools
 import argparse
 from mpl_toolkits.basemap import Basemap
+import networkx as nx
 
 def prepareWorldMapPlot():
     # Create a figure of size (i.e. pretty big)
@@ -130,6 +131,92 @@ def addMostInvolvedFlightsAndConflicts(map, nmax, trajectories, pointConflicts, 
     mostInvolvedFlights = flights2Conflicts.count().T.sort_values('conflictIndex', ascending=False).index[:nmax].values
     addFlightsAndConflicts(map, mostInvolvedFlights, trajectories, pointConflicts, parallelConflicts, flights2Conflicts, blue=True, red=True)
 
+def getPartitions(graph, partition):
+    # number of partitions
+    Np = len(set(partition))
+    # extract nodes from graph
+    nodes = list(set([n1 for n1, n2 in graph] + [n2 for n1, n2 in graph]))
+    # create networkx graphs
+    G = []
+    for n in range(Np):
+        G.append(nx.Graph())
+    # add nodes
+    for node, p in zip(nodes, partition):
+        G[p].add_node(node)
+    # add edges
+    for edge in graph:
+        if partition[nodes.index(edge[0])] == partition[nodes.index(edge[1])]:
+            G[partition[nodes.index(edge[0])]].add_edge(edge[0], edge[1])
+    return G
+
+def plotConflictGraph(pointConflicts, parallelConflicts, nparts=None, partition=None, grid=False, separate=False):
+    """ Plot the conflicts as a graph with flights as nodes and conflicts as edges
+
+    Arguments:
+        pointConflicts: Pandas Dataframe containing the point conflicts
+        parallelConflicts: Pandas Dataframe containing the parallel conflicts
+    """
+    # get edge tuples defining a graph
+    l = pd.concat([parallelConflicts.loc[:, ['flight1', 'flight2']], pointConflicts.loc[:, ['flight1', 'flight2']]]).values.tolist()
+    # convert to networkx format
+    # extract nodes from graph
+    nodes = set([n1 for n1, n2 in l] + [n2 for n1, n2 in l])
+    # create networkx graph
+    G = nx.Graph()
+    # add nodes
+    for node in nodes:
+        G.add_node(node)
+    # add edges
+    for edge in l:
+        G.add_edge(edge[0], edge[1])
+
+    if not nparts:
+        nx.draw(G, pos=nx.spring_layout(G), node_size=100)
+    else:
+        try:
+            import metis
+        except:
+            print "Unable to plot partitioned graph without metis installed"
+            raise
+
+        p = metis.part_graph(G, nparts=nparts)
+        partition_color = np.array(p[1])
+        if not grid and not separate:
+            if partition:
+                assert(partition < nparts)
+                partition_color = partition_color == partition
+            else:
+                partition_color = np.array(partition_color)/float(nparts)
+            nx.draw(G, node_color=partition_color, node_size=100)
+        elif grid:
+            fig = plt.figure(figsize=(6, 3*nparts))
+            # initial positioning
+            init_pos = nx.spring_layout(G)
+            ax = []
+            for i in range(nparts):
+                ax.append(fig.add_subplot(int(0.5 * nparts), 2, i + 1))
+                partition_color = np.array(p[1])
+                partition_color_i = partition_color == i
+                nx.draw(G, node_color=partition_color_i, node_size=100, ax=ax[i], pos=init_pos)
+        elif separate:
+            graphs = getPartitions(l, p[1])
+            partition_color = np.array(p[1])
+            # partition_color = np.array(partition_color)/float(nparts)
+            perm = np.random.permutation(nparts)
+            partition_color = np.apply_along_axis(lambda x: perm[x], axis=0, arr=partition_color)
+            layout = {}
+            nrow = 3
+            ncol = 5
+            nmulti = (nparts - nparts % (nrow * ncol)) / (nrow * ncol) + 1
+            nrows = nmulti * nrow
+            scale = 2
+            for n in range(nparts):
+                xpos = scale * (n % nrows)
+                ypos = scale * (n - n % nrows) / nrows
+                d = nx.spring_layout(graphs[n], center=(xpos, ypos))
+                layout = dict(layout.items() + d.items())
+            nx.draw(G, node_size=100, pos=layout, node_color=partition_color)
+
 def main():
     parser = argparse.ArgumentParser(description='Calculate point conflicts from trajectory data')
     subparsers = parser.add_subparsers(help="Give a keyword", dest='mode')
@@ -152,6 +239,15 @@ def main():
     flight_parser.add_argument('--point_conflict_file', default='data/TrajDataV2_20120729.txt.pointConflicts.csv', help='input file containing the point conflicts')
     flight_parser.add_argument('--parallel_conflict_file', default='data/TrajDataV2_20120729.txt.parallelConflicts.csv', help='input file containing the parallel conflicts')
     flight_parser.add_argument('--flights2conflicts_file', default='data/TrajDataV2_20120729.txt.flights2Conflicts.h5', help='input file the mapping from flight to conflict indices')
+
+    graph_parser = subparsers.add_parser("graph", help='Plot a conflicting flights as graph')
+    graph_parser.add_argument('-n', '--nparts', default=None, help='Number of partitions to plot', type=int)
+    group_graph = graph_parser.add_mutually_exclusive_group()
+    group_graph.add_argument('-p', '--partition', default=None, help='Partition to highlight', type=int)
+    group_graph.add_argument('--grid', action='store_true', help='Plot all partitions in multiple plots')
+    group_graph.add_argument('--separate', action='store_true', help='Spatially separate partitions in plot')
+    graph_parser.add_argument('--point_conflict_file', default='data/TrajDataV2_20120729.txt.pointConflicts.csv', help='input file containing the point conflicts')
+    graph_parser.add_argument('--parallel_conflict_file', default='data/TrajDataV2_20120729.txt.parallelConflicts.csv', help='input file containing the parallel conflicts')
 
     args = parser.parse_args()
 
@@ -187,6 +283,11 @@ def main():
             addFlightsAndConflicts(map, [args.flightIndex], trajectories, pointConflicts, parallelConflicts, flights2Conflicts)
         else:
             addMostInvolvedFlightsAndConflicts(map, args.numberOfFlightIndices, trajectories, pointConflicts, parallelConflicts, flights2Conflicts)
+        plt.show()
+    if args.mode == 'graph':
+        pointConflicts = pd.read_csv(args.point_conflict_file, index_col='conflictIndex')
+        parallelConflicts = pd.read_csv(args.parallel_conflict_file, index_col='parallelConflict')
+        plotConflictGraph(pointConflicts, parallelConflicts, nparts=args.nparts, partition=args.partition, separate=args.separate, grid=args.grid)
         plt.show()
 
 if __name__ == "__main__":
