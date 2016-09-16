Fixed energy calculation for annealing runs

code/departureDelayModel/qubo/runInstance.py
@@ -158,7 +158,7 @@ def atm(instancefile, num_embed=1, e=None, use_snapshots=False, embedding_only=F
                 logRawResult = np.load(logRawSolutionFile)
                 energies = np.load(energiesFile)
                 numOccurrences = np.load(numOccurrencesFile)
-            print "Solution has energy: %f" % energies[0]
+            print "Solution has energy: %f" % q.evaluate(logRawResult[0])
             for k, v in subqubos.items():
                 print "Contribution of %s term: %f" % (k, v.evaluate(logRawResult[0]))
 