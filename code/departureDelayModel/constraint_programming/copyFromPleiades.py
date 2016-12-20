3d0a956c42a62bd471f41f838e68d09c79228e85
Tobias Stollenwerk <tobias.stollenwerk@dlr.de>
2016-12-21 00:19:09 +0100

    Added sync script for constraint programming results

---
+++ code/departureDelayModel/constraint_programming/copyFromPleiades.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import subprocess
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='copy data from pleiades cluster')
+    parser.add_argument('-u', '--user', default='tstollen', help='user name on cluster')
+    parser.add_argument('-n', '--host', default='pfe23', help='host name on cluster')
+    parser.add_argument('-p', '--path', default='projects/qcfatm/code/departureDelayModel/constraint_programming/data/', help='path to data folder on cluster')
+    parser.add_argument('-d', '--destination', default='data', help='path to destination data folder on local machine')
+    args = parser.parse_args()
+
+    cmd = "rsync -avz %s@%s:%s %s" % (args.user, args.host, args.path, args.destination)
+    print cmd
+    subprocess.call(cmd, shell=True)
+if __name__ == "__main__":
+    main()
