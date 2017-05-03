f513ce6fc376186a483dce11c31b4cf940a412d9
Tobias Stollenwerk <tobias.stollenwerk@dlr.de>
2017-05-02 17:09:21 -0700

    Added script for QUBO creation

---
+++ code/departureDelayModel/qubo/scripts/createQUBOAllConnectedComponents.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import subprocess
+
+skipBigProblems = 3000
+num_embed = 0
+nProc = 1
+# the maximum delay of the precalculation
+for maxDelayPrecalc in [6, 9, 12, 15, 18, 24, 36, 48, 60]:
+    # get the maximum value of the delay variable (always <= the maximum delay of the precalculation)
+    instanceFolder = '../../../precalculation/data/instances/connectedComponents/maxDepartDelayPrecalculation_%03i' % maxDelayPrecalc
+    # get partition maximum
+    cmd = 'ls %s | tail -n 1' % instanceFolder
+    return_string = subprocess.check_output(cmd, shell=True)
+    partitionMax = int(return_string.partition('partition')[2].split('_')[0])
+    delaySteps = []
+    for n in range(1, maxDelayPrecalc + 1):
+        if maxDelayPrecalc % n == 0:
+            delaySteps.append(str(maxDelayPrecalc / n))
+
+    cmd = ''
+    cmd += './solveConnectedComponents.py'
+    cmd += ' --inventoryfile data/connectedComponents/maxDelayPrecalc%03i/inventory.h5' % maxDelayPrecalc
+    cmd += ' -o data/connectedComponents/maxDelayPrecalc%03i/results' % maxDelayPrecalc
+    cmd += ' --instanceFolder %s/' % instanceFolder
+    cmd += ' --pmax %i' % partitionMax
+    cmd += ' --penalty_weights_two_tuples 1 1'
+    cmd += ' --num_embed %i' % num_embed
+    cmd += ' -d %s' % ' '.join(delaySteps)
+    cmd += ' --skipBigProblems %i' % skipBigProblems
+    cmd += ' --use_snapshots'
+    cmd += ' --qubo_creation_only'
+    cmd += ' --maxDelay %i' % maxDelayPrecalc
+    cmd += ' --np %i' % nProc
+    print cmd
+    subprocess.call(cmd, shell=True)
